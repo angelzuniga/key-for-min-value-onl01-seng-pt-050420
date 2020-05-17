2fa2c69262fc427c15c6762138967cfb38088585
Angel Zuniga <angelzuniga@outlook.com>
2020-05-17 19:03:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,5 @@ def key_for_min_value(hash)
   min_value = hash.min_by {|k,v| v}
   unless min_value == nil 
     min_value.first 
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
   end
 end
