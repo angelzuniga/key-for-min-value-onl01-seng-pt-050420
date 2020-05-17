9f2787106c95da4a4b73dd88915ddc20782f2d23
Angel Zuniga <angelzuniga@outlook.com>
2020-05-17 18:59:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ min_value = hash.min_by {|key,value| value}
+  unless min_value == nil 
+    min_value.first 
+  end
 end
