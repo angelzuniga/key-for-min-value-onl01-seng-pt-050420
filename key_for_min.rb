4e296c428f177cf083bb337bcb87229838ae3b00
Angel Zuniga <angelzuniga@outlook.com>
2020-05-17 19:22:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,3 +7,15 @@
 #     min_value.first
 #   end
 # end
+
+def key_for_min_value(name_hash)
+  lowest_key = nil 
+  lowest_value = nil 
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v 
+      lowest_key = k 
+    end 
+  end 
+  lowest_key
+end 
